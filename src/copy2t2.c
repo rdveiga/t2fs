e17e4ca93a37754fa127169826c2988bdf86b7fb
Gabriel <gstding@gmail.com>
2014-06-29 22:36:11 -0300

    implementacao de copy2t2.c

--- src/copy2t2.c
+++ src/copy2t2.c
@@ -1,9 +1,26 @@
-/*
-	Realiza a cópia de um arquivo qualquer do sistema de arquivos nativo da máquina para o disco lógico do T2FS.
-O caminho de destino no disco T2FS é absoluto. Exemplo de uso
-	%copy2t2 ./disciplina/inf01142/t2fs.pdf /aluno/sisop/trabalhopratico2.pdf
+#include "t2fs.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-	Copia o arquivo t2fs.pdf, que se encontra no sistema de arquivos nativo da máquina (diretório disciplina,
-subdiretório inf01142), para o disco lógico T2FS, com o nome trabalhopratico2.pdf. Se os diretórios de
-destino, aluno ou sisop, não existirem esse comando fornece uma mensagem de erro.
-*/
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		printf("Invalid arguments");
+		return !;
+	} else {
+		FILE *f = fopen(argv[0], "r");
+		fseek(f, 0, SEEK_END);
+		long fsize = ftell(f);
+		fseek(f, 0, SEEK_SET);
+
+		char *buffer = malloc(fsize + 1);
+		fread(buffer, fsize, 1, f);
+		fclose(f);
+
+		buffer[fsize] = 0;
+
+		t2fs_file handle = t2fs_create(argv[1]);
+		int t2fsize = t2fs_write(handle, buffer, fsize);
+
+		return 0;
+	}
+}
