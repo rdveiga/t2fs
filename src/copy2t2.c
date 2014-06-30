f0a30f75d89b908a02e4cd1f35e873dab2d7134f
Gabriel <gstding@gmail.com>
2014-06-29 22:44:32 -0300

    pequena modif em copy2t2.c de novo

--- src/copy2t2.c
+++ src/copy2t2.c
@@ -1,6 +1,13 @@
 /* 
-	Realiza a cópia de um arquivo qualquer do sistema de arquivos nativo da máquina para o disco lógico do T2FS. O caminho de destino no disco T2FS é absoluto. Exemplo de uso %copy2t2 ./disciplina/inf01142/t2fs.pdf /aluno/sisop/trabalhopratico2.pdf
-Copia o arquivo t2fs.pdf, que se encontra no sistema de arquivos nativo da máquina (diretório disciplina, subdiretório inf01142), para o disco lógico T2FS, com o nome trabalhopratico2.pdf. Se os diretórios de destino, aluno ou sisop, não existirem esse comando fornece uma mensagem de erro.
+    Realiza a cópia de um arquivo qualquer do sistema de arquivos nativo 
+da máquina para o disco lógico do T2FS. O caminho de destino no disco 
+T2FS é absoluto. Exemplo de uso 
+%copy2t2 ./disciplina/inf01142/t2fs.pdf /aluno/sisop/trabalhopratico2.pdf
+
+    Copia o arquivo t2fs.pdf, que se encontra no sistema de arquivos nativo 
+da máquina (diretório disciplina, subdiretório inf01142), para o disco 
+lógico T2FS, com o nome trabalhopratico2.pdf. Se os diretórios de destino, 
+aluno ou sisop, não existirem esse comando fornece uma mensagem de erro.
 */
 
 #include "t2fs.h"
